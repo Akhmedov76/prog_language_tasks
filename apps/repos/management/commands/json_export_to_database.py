263f9809c9ba172ce1e605e82b2a829fc782dd81
Jasurbek Ahmedov <ahmedovj7686@gmail.com>
2025-09-17 09:25:00 +0500

    upd

---
+++ apps/repos/management/commands/json_export_to_database.py
@@ -0,0 +1,183 @@
+import json
+
+from django.core.management.base import BaseCommand
+
+from apps.language.models import Language
+from apps.repos.models import Repository, RepositoryLanguage, TopicsStars
+from apps.topics.models import Topic
+
+BATCH_SIZE = 5000
+
+
+class Command(BaseCommand):
+    help = "JSON import qilish"
+
+    def add_arguments(self, parser):
+        parser.add_argument("json_path", type=str, help="JSON fayl manzili")
+
+    def handle(self, *args, **opts):
+        path = opts["json_path"]
+
+        with open(path, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            repos = []
+
+            for record in data:
+                owner, _ = Repository.objects.get_or_create(username=record.get("owner"))
+                primary_lang, _ = Language.objects.get_or_create(
+                    name=record.get("primaryLanguage")
+                )
+                repo = Repository(
+                    owner=owner,
+                    name=record.get("name"),
+                    stars=record.get("stars"),
+                    forks=record.get("forks"),
+                    watchers=record.get("watchers"),
+                    isFork=record.get("isFork"),
+                    isArchived=record.get("isArchived"),
+                    languagesCount=record.get("languagesCount"),
+                    topicCount=record.get("topicCount"),
+                    diskUsageKb=record.get("diskUsageKb"),
+                    pullRequests=record.get("pullRequests"),
+                    issues=record.get("issues"),
+                    description=record.get("description"),
+                    primaryLanguage=primary_lang,
+                    createdAt=record.get("createdAt"),
+                    pushedAt=record.get("pushedAt"),
+                    defaultBranchCommitCount=record.get("defaultBranchCommitCount"),
+                    license=record.get("license"),
+                    assignableUserCount=record.get("assignableUserCount"),
+                    codeOfConduct=record.get("codeOfConduct"),
+                    forkingAllowed=record.get("forkingAllowed"),
+                    nameWithOwner=record.get("nameWithOwner"),
+                    parent=record.get("parent"),
+                )
+                repo.save()
+
+                for lang in record.get("languages", []):
+                    lang_obj, _ = Language.objects.get_or_create(
+                        name=lang.get("name")
+                    )
+                    RepositoryLanguage.objects.create(
+                        repo=repo,
+                        language=lang_obj,
+                        size=lang.get("size"),
+                    )
+
+                for topic in record.get("topics", []):
+                    topic_obj, _ = Topic.objects.get_or_create(name=topic.get("topic"))
+                    TopicsStars.objects.create(
+                        repo=repo,
+                        topic=topic_obj,
+                        stars=topic.get("stars"),
+                    )
+
+                repos.append(repo)
+
+                if len(repos) >= BATCH_SIZE:
+                    self.stdout.write(f"{len(repos)} repo import qilindi.")
+                    repos.clear()
+
+        self.stdout.write(self.style.SUCCESS("Import tugadi"))
+
+# import json
+#
+# from django.core.management.base import BaseCommand
+# from django.db import transaction
+#
+# from apps.language.models import Language
+# from apps.repos.models import Repository, RepositoryLanguage, TopicsStars
+# from apps.topics.models import Topic
+#
+#
+# class Command(BaseCommand):
+#     help = "Import repositories from JSON file"
+#
+#     def add_arguments(self, parser):
+#         parser.add_argument("json_file", type=str, help="Path to JSON file")
+#
+#     def handle(self, *args, **options):
+#         json_file = options["json_file"]
+#
+#         with open(json_file, "r", encoding="utf-8") as f:
+#             data = json.load(f)
+#
+#         with transaction.atomic():
+#             languages = {l.name: l for l in Language.objects.all()}
+#             topics = {t.name: t for t in Topic.objects.all()}
+#
+#             repos = []
+#             repo_languages = []
+#             repo_topics = []
+#             for repo_data in data:
+#                 print("Processing repo {}".format(repo_data["name"]))
+#                 repo = Repository(
+#                     owner=repo_data["owner"],
+#                     name=repo_data["name"],
+#                     stars=repo_data["stars"],
+#                     forks=repo_data["forks"],
+#                     watchers=repo_data["watchers"],
+#                     is_fork=repo_data["isFork"],
+#                     is_archived=repo_data["isArchived"],
+#                     language_count=repo_data["languageCount"],
+#                     topic_count=repo_data["topicCount"],
+#                     disk_usage_kb=repo_data["diskUsageKb"],
+#                     pull_requests=repo_data["pullRequests"],
+#                     created_at=repo_data.get("createdAt"),
+#                     pushed_at=repo_data.get("pushedAt"),
+#                     issues=repo_data["issues"],
+#                     description=repo_data.get("description"),
+#                     primary_language=repo_data.get("primaryLanguage"),
+#                     default_branch_commit_count=repo_data.get("defaultBranchCommitCount", 0),
+#                     license=repo_data.get("license"),
+#                     assignable_user_count=repo_data.get("assignableUserCount", 0),
+#                     code_of_conduct=repo_data.get("codeOfConduct"),
+#                     forking_allowed=repo_data["forkingAllowed"],
+#                     name_with_owner=repo_data["nameWithOwner"],
+#                     parent=repo_data.get("parent"),
+#                 )
+#                 repos.append(repo)
+#
+#             Repository.objects.bulk_create(repos, ignore_conflicts=True)
+#             created_repos = Repository.objects.filter(
+#                 name_with_owner__in=[r.name_with_owner for r in repos]
+#             )
+#             repo_map = {r.name_with_owner: r for r in created_repos}
+#
+#             repo_obj = repo_map[repo_data["nameWithOwner"]]
+#
+#             print("language qoshilmoqda")
+#
+#             for lang_data in repo_data.get("languages", []):
+#                 lang_name = lang_data["name"]
+#                 if lang_name in languages:
+#                     lang_obj = languages[lang_name]
+#                 else:
+#                     lang_obj = Language.objects.create(name=lang_name)
+#                     languages[lang_name] = lang_obj
+#                 repo_languages.append(RepositoryLanguage(
+#                     repository=repo_obj,
+#                     language=lang_obj,
+#                     size=lang_data.get("size", 0)
+#                 ))
+#
+#             print("topic qoshilmoqda")
+#
+#             for topic_data in repo_data.get("topics", []):
+#                 topic_name = topic_data["name"]
+#                 if topic_name in topics:
+#                     topic_obj = topics[topic_name]
+#                 else:
+#                     topic_obj = Topic.objects.create(name=topic_name)
+#                     topics[topic_name] = topic_obj
+#                 repo_topics.append(TopicsStars(
+#                     repository=repo_obj,
+#                     topic=topic_obj,
+#                     stars=topic_data.get("stars", 0)
+#                 ))
+#
+#         Repository.objects.bulk_create(repos, ignore_conflicts=True)
+#         RepositoryLanguage.objects.bulk_create(repo_languages, ignore_conflicts=True)
+#         TopicsStars.objects.bulk_create(repo_topics, ignore_conflicts=True)
+#
+#         self.stdout.write(self.style.SUCCESS("Import tugadil"))
